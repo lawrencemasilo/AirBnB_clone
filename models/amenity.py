42df2430032a06a5920e63e603a88320324af4d0
Simamnkele <smambo85@gmail.com>
2023-10-10 23:46:01 +0200

    Created rest of classes

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""import modules for class"""
+from model.base_model import BaseModel
+
+
+"""
+Below class names amenities.
+"""
+
+
+class Amenity(BaseModel):
+    """Created instance of class."""
+    name = ""
---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Import modules for class."""
+from models.base_models import BaseModel
+
+
+"""
+Below class for City names.
+"""
+
+
+class City(BaseModel):
+    """Created instance of class."""
+    state_id = ""
+    name = ""
